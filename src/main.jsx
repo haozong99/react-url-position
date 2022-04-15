60c292e6d7e69121541c27ee34bbdbeb139a450d
SEKORM\haozong_yang <haozong9999.@163.com>
2022-04-15 17:23:22 +0800

    增加了图片

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,30 @@
-
+import imgUrl from '../assets/链接.png'
 import {data} from './data';
+import './style.css'
+const {useState} = React
 function Title(props){
+    const [img , setImg] = useState(imgUrl)
+    const setUrl = (kkey) =>{
+        console.log(2);
+        var homeUrl = document.URL;
+        const state = { 'page_id': 1, 'user_id': 5 }
+        const title = ''
+        const url = ''
+
+        history.pushState(state, title, url)
+    }
     return <div>
+        <div className='title' onClick={setUrl(props.title)}>
+            <img src={img}/>
             <h1>
             {props.title}
-        </h1>
-        <p>
-        {props.content}
-        </p>
+            </h1>
+        </div>
+            <p>
+            {props.content}
+            </p>
     </div>
-    
+
 }
 
 function CP(){
